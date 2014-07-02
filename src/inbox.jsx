e56c858d7463ae0cf5643323e0658874266dd66f
toran billups <toranb@gmail.com>
2014-07-02 16:15:43 -0500

    updated inbox to use promise-mixin-two again

--- src/inbox.jsx
+++ src/inbox.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import AjaxMixin from 'ajax-mixin';
+import AnotherPromiseMixin from './mixin';
 
 var Inbox = React.createClass({
+    mixins: [AnotherPromiseMixin],
     render: function() {
+        this.gotime();
         var mixin = new AjaxMixin();
         mixin.xhr("/api/incoming", "GET").then(function(response) {
             response.forEach(function(item) {
